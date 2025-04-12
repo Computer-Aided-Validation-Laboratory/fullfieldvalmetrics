Fixed error in MAVM calculation for wrong probability in papers pseudo code

scripts/main_disp_eval_sim_v2.py
@@ -207,7 +207,7 @@ def main() -> None:
     sim_y_min = np.min(sim_coords[:,1])
     sim_y_max = np.max(sim_coords[:,1])
 
-    plot_disp_sim_exp = True
+    plot_disp_sim_exp = False
     if plot_disp_sim_exp:
         frame = 500
         div_n = 1000
@@ -263,7 +263,7 @@ def main() -> None:
     # NOTE: coords are flipped compared to plotted maps above!
     # EXPERIMENT STEADY STATE: 300-650
 
-    plot_disp_traces = True
+    plot_disp_traces = False
     if plot_disp_traces:
         ax_ind: int = 0
         fig,ax = plt.subplots()
@@ -436,13 +436,13 @@ def main() -> None:
                                exp_disp_common[:,mavm_inds[ax_str][0],yy])
 
 
-    print(80*"-")
-    print(f"{mavm_inds['x']=}")
-    print(f"{mavm_inds['y']=}")
-    print(f"{coords_common[mavm_inds['x'],:]=}")
-    print(f"{coords_common[mavm_inds['y'],:]=}")
-    print(80*"-")
-    print()
+    # print(80*"-")
+    # print(f"{mavm_inds['x']=}")
+    # print(f"{mavm_inds['y']=}")
+    # print(f"{coords_common[mavm_inds['x'],:]=}")
+    # print(f"{coords_common[mavm_inds['y'],:]=}")
+    # print(80*"-")
+    # print()
 
     plot_mavm = True
     if plot_mavm:
@@ -460,23 +460,25 @@ def main() -> None:
               field_label,
               save_tag=SIM_TAG)
 
-    print(80*"-")
-    print(f"{type(mavm_res['x']['d+'])=}")
-    print(f"{type(mavm_res['x']['d-'])=}")
-    print(f"{type(mavm_res['y']['d+'])=}")
-    print(f"{type(mavm_res['y']['d-'])=}")
-    print()
-    print(f"{mavm_res['x']['d+']=}")
-    print(f"{mavm_res['x']['d-']=}")
-    print(f"{mavm_res['y']['d+']=}")
-    print(f"{mavm_res['y']['d-']=}")
-    print(80*"-")
+    # print(80*"-")
+    # print(f"{type(mavm_res['x']['d+'])=}")
+    # print(f"{type(mavm_res['x']['d-'])=}")
+    # print(f"{type(mavm_res['y']['d+'])=}")
+    # print(f"{type(mavm_res['y']['d-'])=}")
+    # print()
+    # print(f"{mavm_res['x']['d+']=}")
+    # print(f"{mavm_res['x']['d-']=}")
+    # print(f"{mavm_res['y']['d+']=}")
+    # print(f"{mavm_res['y']['d-']=}")
+    # print(80*"-")
+
     #---------------------------------------------------------------------------
     # Calculate the mavm d+,d- full-field
+    force_mavm_map = True
     mavm_d_plus_path = temp_path / f"mavm_d_plus_{SIM_TAG}.npy"
     mavm_d_minus_path = temp_path / f"mavm_d_minus_{SIM_TAG}.npy"
 
-    if not mavm_d_plus_path.is_file() and not mavm_d_minus_path.is_file():
+    if force_mavm_map or (not mavm_d_plus_path.is_file() and not mavm_d_minus_path.is_file()):
         print("Calculating MAVM d+ and d- over all points for all disp comps.")
         mavm_d_plus = np.zeros((grid_pts,3))
         mavm_d_minus = np.zeros((grid_pts,3))

scripts/valmetrics.py
@@ -394,8 +394,6 @@ def interp_exp_to_common_grid(coords: np.ndarray,
 
     return disp_common
 
-
-
 #-------------------------------------------------------------------------------
 # MAVM Calculation
 def mavm(model_data,
@@ -407,107 +405,224 @@ def mavm(model_data,
     Downloaded from http://asmedigitalcollection.asme.org/verification/article-pdf/8/1/011001/6974199/vvuq_008_01_011001.pdf on 24 May 2024.
     """
 
+    S_num_mod = model_data.shape[0]
+    N_num_exp = exp_data.shape[0]
+
     # find empirical cdf
     model_cdf = stats.ecdf(model_data).cdf
     exp_cdf = stats.ecdf(exp_data).cdf
 
-    F_ = model_cdf.quantiles
-    Sn_ = exp_cdf.quantiles
+    F_mod_vec = model_cdf.quantiles
+    Sn_exp_vec = exp_cdf.quantiles
 
-    df = len(Sn_)-1
+    df = len(Sn_exp_vec)-1
     t_alph = stats.t.ppf(0.95,df)
 
-    Sn_conf = [Sn_ - t_alph*(np.nanstd(Sn_)/np.sqrt(len(Sn_))),
-               Sn_ + t_alph*(np.nanstd(Sn_)/np.sqrt(len(Sn_)))]
+    Sn_conf_exp_list = [Sn_exp_vec - t_alph*(np.nanstd(Sn_exp_vec)/np.sqrt(N_num_exp)),
+               Sn_exp_vec + t_alph*(np.nanstd(Sn_exp_vec)/np.sqrt(N_num_exp))]
 
-    Sn_Y = exp_cdf.probabilities
-    F_Y = model_cdf.probabilities
+    Sn_Y_exp_vec = exp_cdf.probabilities
+    F_Y_mod_vec = model_cdf.probabilities
 
-    P_F = 1/len(F_)
-    P_Sn = 1/len(exp_cdf.quantiles)
+    p_F_mod_scalar = 1/S_num_mod
+    p_Sn_exp_scalar = 1/N_num_exp
 
-    d_conf_plus = []
-    d_conf_minus = []
+    # print(80*"=")
+    # print(f"{model_data.shape=}")
+    # print(f"{model_cdf.quantiles.shape=}")
+    # print(f"{model_cdf.probabilities.shape=}")
+    # print()
+    # print(f"{exp_data.shape=}")
+    # print(f"{exp_cdf.quantiles.shape=}")
+    # print(f"{exp_cdf.probabilities.shape=}")
+    # print()
+
+    # print("EXP QUANTS")
+    # print(exp_cdf.quantiles)
+    # print("EXP PROBS")
+    # print(exp_cdf.probabilities)
+    # print("EXP CONF UP")
+    # print(Sn_conf_exp_list[1])
+    # print("EXP CONF LOW")
+    # print(Sn_conf_exp_list[0])
+    # print()
+    # print("MODEL QUANTS")
+    # print(model_cdf.quantiles)
+    # print("MODEL PROBS")
+    # print(model_cdf.probabilities)
+    # print()
+    # print(80*"=")
+
+    # (fig,ax) = plt.subplots()
+    # plt.plot(model_cdf.quantiles,model_cdf.probabilities)
+    # plt.show()
+
+    d_conf_plus: list = []
+    d_conf_minus: list = []
+
+    tol = 1e-12
 
     for k in [0,1]:
+        # print(80*"=")
+        # print(f"CONFIDENCE BOUND, k: {k}")
+        # print()
 
-        ii = 0
-        d_rem = 0
+        # USed to step through the longer of experiment vs simulation
+        ii: int = 0
 
-        d_plus = 0
-        d_minus = 0
+        #
+        d_rem: float = 0.0
 
-        Sn = Sn_conf[k]
+        d_plus: float = 0.0
+        d_minus: float = 0.0
+
+        # Grab the lower [0] or upper [1] bound of the confidence interval
+        Sn_exp_vec = Sn_conf_exp_list[k]
 
         #If more experimental data points than model data points
-        if len(Sn) > len(F_):
+        if N_num_exp > S_num_mod:
+            #print(80*"=")
+            # We step through the simulation as we have less simulation points
+            for jj in range(len(F_mod_vec)):
 
-            for jj in range(0,len(F_)):
-                if d_rem != 0:
-                    d_ = (Sn[ii] - F_[jj]) * (P_Sn*(ii+1) - P_F*jj)
-                    if d_ > 0:
-                        d_plus += d_
+                # print(80*"-")
+                # print(f"Model: {jj}")
+                # print(f"{d_rem=}")
+                # print(f"{((d_rem >= tol) or (d_rem <= -tol))=}")
+                # print(80*"-")
+                # print()
+
+                if (d_rem >= tol) or (d_rem <= -tol): # Fixed floating point issues here
+
+                    d_ii = ((Sn_exp_vec[ii] - F_mod_vec[jj])
+                          * (p_Sn_exp_scalar*(ii+1) - p_F_mod_scalar*jj))
+
+                    if d_ii > 0:
+                        d_plus += d_ii
                     else:
-                        d_minus += d_
-                    ii += 1
-                while (jj+1)*P_F > (ii+1)*P_Sn:
-                    d_ = (Sn[ii] - F_[jj])*P_F
-                    if d_ > 0:
-                        d_plus += d_
-                    else:
-                        d_minus += d_
+                        d_minus += d_ii
+
+                    # print("REMAINDER BLOCK")
+                    # print(f"Experiment: {ii}")
+                    # print(f"{Sn_exp_vec[ii]=}")
+                    # print(f"{F_mod_vec[jj]=}")
+                    # print(f"{(Sn_exp_vec[ii] - F_mod_vec[jj])=}")
+                    # print()
+                    # print(f"{(p_Sn_exp_scalar*(ii+1))=}")
+                    # print(f"{(p_F_mod_scalar*jj)=}")
+                    # print(f"{(p_Sn_exp_scalar*(ii+1) - p_F_mod_scalar*jj)=}")
+                    # print()
+                    # print(f"{d_ii=}")
+                    # print(f"{d_plus=}")
+                    # print(f"{d_minus=}")
+                    # print()
 
                     ii += 1
-                d_rem = (Sn[ii]-F_[jj])*(P_F*(jj+1) - P_Sn*ii)
-                if d_rem > 0:
+
+                # While model prob is greater than experimental prob do:
+                # This steps through the experiment until we hit the next simulation
+                while (jj+1)*p_F_mod_scalar > (ii+1)*p_Sn_exp_scalar:
+                    # Difference between exp conf bound and simulation multiplied
+                    # by the simulation probability = Area
+                    # NOTE: ERROR HERE - should be experiment prob
+                    # d_ii = (Sn_exp_vec[ii] - F_mod_vec[jj])*p_F_mod_scalar
+                    d_ii = (Sn_exp_vec[ii] - F_mod_vec[jj])*p_Sn_exp_scalar
+
+                    if d_ii > 0:
+                        d_plus += d_ii
+                    else:
+                        d_minus += d_ii
+
+                    # print("WHILE BLOCK:")
+                    # print(f"Experiment: {ii}")
+                    # print(f"{Sn_exp_vec[ii]=}")
+                    # print(f"{F_mod_vec[jj]=}")
+                    # print(f"{(Sn_exp_vec[ii] - F_mod_vec[jj])=}")
+                    # print(f"{p_F_mod_scalar=}")
+                    # print(f"{p_Sn_exp_scalar=}")
+                    # print()
+                    # print(f"{d_ii=}")
+                    # print(f"{d_plus=}")
+                    # print(f"{d_minus=}")
+                    # print()
+
+                    ii += 1
+
+                d_rem = (Sn_exp_vec[ii]-F_mod_vec[jj])*(p_F_mod_scalar*(jj+1) - p_Sn_exp_scalar*ii)
+
+                if d_rem > 0.0:
                     d_plus += d_rem
                 else:
                     d_minus += d_rem
+
+            #     print("FINAL BLOCK:")
+            #     print(f"{Sn_exp_vec[ii]=}")
+            #     print(f"{F_mod_vec[jj]=}")
+            #     print(f"{(Sn_exp_vec[ii] - F_mod_vec[jj])=}")
+            #     print()
+
+            #     print(f"{d_plus=}")
+            #     print(f"{d_minus=}")
+            #     print(f"{d_rem=}")
+
+            # print()
+            # print(80*"=")
 
         #If more model data points than experimental data points (more typical)
-        elif len(Sn) <= len(F_):
+        elif len(Sn_exp_vec) <= len(F_mod_vec):
 
-            for jj in range(0,len(Sn)):
+            for jj in range(0,len(Sn_exp_vec)):
 
-                if d_rem != 0:
-                    d_ = (Sn[jj]-F_[ii])*(P_F*(ii+1) - P_Sn*jj)
-                    if d_ > 0:
-                        d_plus += d_
+                if (d_rem >= tol) or (d_rem <= -tol):
+                    d_ii = (Sn_exp_vec[jj]-F_mod_vec[ii])*(p_F_mod_scalar*(ii+1) - p_Sn_exp_scalar*jj)
+                    if d_ii > 0:
+                        d_plus += d_ii
                     else:
-                        d_minus += d_
+                        d_minus += d_ii
                     ii += 1
 
-                while (ii+1)*P_F < (jj+1)*P_Sn:
-                    d_ = (Sn[jj]-F_[ii])*P_F
-                    if d_ > 0:
-                        d_plus += d_
+                while (ii+1)*p_F_mod_scalar < (jj+1)*p_Sn_exp_scalar:
+                    d_ii = (Sn_exp_vec[jj]-F_mod_vec[ii])*p_F_mod_scalar
+                    if d_ii > 0:
+                        d_plus += d_ii
                     else:
-                        d_minus += d_
+                        d_minus += d_ii
 
                     ii += 1
 
-                d_rem = (Sn[jj]-F_[ii])*(P_Sn*(jj+1) - P_F*ii)
+                d_rem = (Sn_exp_vec[jj]-F_mod_vec[ii])*(p_Sn_exp_scalar*(jj+1) - p_F_mod_scalar*ii)
                 if d_rem > 0:
                     d_plus += d_rem
                 else:
                     d_minus += d_rem
 
+        # This is a two element list
         d_conf_plus.append(np.abs(d_plus))
         d_conf_minus.append(np.abs(d_minus))
+
+
 
     d_plus = np.nanmax(d_conf_plus)
     d_minus = np.nanmax(d_conf_minus)
 
+    # print()
+    # print("CALCULATION END")
+    # print(f"{d_conf_plus=}")
+    # print(f"{d_conf_minus=}")
+    # print(f"{d_plus=}")
+    # print(f"{d_minus=}")
+    # print()
 
     output_dict = {"model_cdf":model_cdf,
                    "exp_cdf":exp_cdf,
                    "d+":d_plus,
                    "d-":d_minus,
-                   "Sn_conf":Sn_conf,
-                   "F_":F_,
-                   "F_Y":F_Y,}
+                   "Sn_conf":Sn_conf_exp_list,
+                   "F_":F_mod_vec,
+                   "F_Y":F_Y_mod_vec,}
 
     return output_dict
+
 
 def mavm_figs(mavm_res: dict[str,Any],
               title_str: str,
@@ -561,7 +676,7 @@ def mavm_figs(mavm_res: dict[str,Any],
     axs.plot(F_+d_plus,F_Y,"k--")
     axs.plot(F_-d_minus,F_Y,"k--")
     axs.fill_betweenx(F_Y,F_-d_minus,F_+d_plus,color="k",alpha=0.2)
-    
+
     # axs.plot(F_,F_Y,"k-")
     # axs.plot(d_plus,F_Y,"k--")
     # axs.plot(d_minus,F_Y,"k--")